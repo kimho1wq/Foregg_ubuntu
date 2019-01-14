1aa0c384c46b47a45967822f8e9d45b007d98f12
seung-bin <kimho1wq@naver.com>
2019-01-14 17:00:50 +0900

    content video link add

--- routes/content.js
+++ routes/content.js
@@ -5,31 +5,50 @@ var async = require('async');
 router.get('/',function(req, res, next) {
     console.log('/content get pass request.');
     var content_id = req.query.id;
-    
+  
     async.waterfall([
 		function (callback) {
             var resultJson = {
                 result : true,
                 message : '',
-                matchData: null
+                matchData: null,
+                match_videoData: null
             };
             
             pool.query('SELECT * FROM users u, match_contents mc, match_contents_type mct WHERE mc.match_id = ? AND mc.match_writer = u.user_uid AND mc.match_type = mct.type', [content_id], (err, rows) => {
                 if (err) {
                     console.log(err);
                     resultJson.result = false;
-                    resultJson.message = '글 정보를 찾지 못하였습니다.';
+                    resultJson.message = 'DB SELECT ERROR1';
                     callback(null, resultJson);
                 } else {
                     resultJson.matchData = rows[0];
                     callback(null, resultJson);
                 }
             });    
+		},
+        function (resultJson, callback) {
+            
+             if(resultJson.result) {
+                pool.query('SELECT match_id, match_video_link, match_create_date FROM match_contents mc, match_contents_video mcv WHERE mc.match_id = ? AND mc.match_id = mcv.match_video_id ORDER BY match_create_date DESC', [content_id] (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        resultJson.result = false;
+                        resultJson.message = 'DB SELECT ERROR2';
+                        callback(null, resultJson);
+                    } else {
+                        resultJson.match_videoData = rows;
+                        callback(null, resultJson);
+                    }
+                });
+            } else {
+                callback(null, resultJson);
+            }
 		}
 	],
     function (callback, resultJson) {
         if(resultJson.result) { 
-            res.render('content', { login : req.session.user, matchData: resultJson.matchData });
+            res.render('content', { login : req.session.user, matchData: resultJson.matchData, match_videoData: resultJson.match_videoData });
         } else {
             res.send('<script type="text/javascript">alert("'+ resultJson.message +'");window.location.href = "/editor";</script>');
         }
