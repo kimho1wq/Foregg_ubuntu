3ef4489fb94cccb6fb6428a26070f1925e372670
Geonho Kim <gunhoflash@gmail.com>
2019-01-26 00:46:13 +0900

    Edit header

--- public/js/common.js
+++ public/js/common.js
@@ -12,36 +12,36 @@ $(document).ready(function ()
 		alert("[" + $(this).text() + "]\nComing soon");
 	});
 	
-	$(document).on('scroll', function ()
-	{
-		if ($("#header").length)
-		{
-			if (window.scrollY > 20)
-			{
-				$("#header")
-					.addClass('scrolled')
-					.addClass('navbar-light')
-					.removeClass('navbar-dark')
-					.addClass('bg-light')
-					.removeClass('bg-dark')
-					.addClass('border-bottom');
-				$("#header .btn-outline-light")
-					.addClass('btn-outline-dark')
-					.removeClass('btn-outline-light');
-			}
-			else
-			{
-				$("#header")
-					.removeClass('scrolled')
-					.removeClass('navbar-light')
-					.addClass('navbar-dark')
-					.removeClass('bg-light')
-					.addClass('bg-dark')
-					.removeClass('border-bottom');
-				$("#header .btn-outline-dark")
-					.addClass('btn-outline-light')
-					.removeClass('btn-outline-dark');
-			}
-		}
-	});
+	// $(document).on('scroll', function ()
+	// {
+	// 	if ($("#header").length)
+	// 	{
+	// 		if (window.scrollY > 20)
+	// 		{
+	// 			$("#header")
+	// 				.addClass('scrolled')
+	// 				.addClass('navbar-light')
+	// 				.removeClass('navbar-dark')
+	// 				.addClass('bg-light')
+	// 				.removeClass('bg-dark')
+	// 				.addClass('border-bottom');
+	// 			$("#header .btn-outline-light")
+	// 				.addClass('btn-outline-dark')
+	// 				.removeClass('btn-outline-light');
+	// 		}
+	// 		else
+	// 		{
+	// 			$("#header")
+	// 				.removeClass('scrolled')
+	// 				.removeClass('navbar-light')
+	// 				.addClass('navbar-dark')
+	// 				.removeClass('bg-light')
+	// 				.addClass('bg-dark')
+	// 				.removeClass('border-bottom');
+	// 			$("#header .btn-outline-dark")
+	// 				.addClass('btn-outline-light')
+	// 				.removeClass('btn-outline-dark');
+	// 		}
+	// 	}
+	// });
 });
